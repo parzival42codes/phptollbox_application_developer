1860d254df31f2158424d151eb2285c02bd581f5
Stefan Schlombs <stefan.schlombs@gmail.com>
2021-11-06 12:44:44 +0100

    Skeleton Console

--- Skeleton/Skeleton.console.php
+++ Skeleton/Skeleton.console.php
@@ -10,8 +10,78 @@ class ApplicationDeveloperSkeleton_console extends Console_abstract
 
         if (file_exists($path)) {
             {
+
                 $fileContent     = file_get_contents($path);
-                $fileContentJson = json_decode($fileContent);
+                $fileContentJson = json_decode($fileContent,
+                                               true);
+
+//                d($fileContentJson);
+
+                $this->addProgressFunction(function ($progressData) {
+
+                    /* $data */
+
+                    $templateCache = new ContainerExtensionTemplateLoad_cache_template('ApplicationDeveloperSkeleton',
+                                                                                       'classdoc');
+
+                    d(Core::getRootClass(__CLASS__));
+                    d($templateCache);
+
+                    $templateClassDoc = new ContainerExtensionTemplate();
+                    $templateClassDoc->set($templateCache->get()['classdoc']);
+                    $templateClassDoc->assign('name',
+                        ($data['name'] ?? ''));
+                    $templateClassDoc->assign('description',
+                        ($data['description'] ?? ''));
+                    $templateClassDoc->assign('author',
+                        ($data['author'] ?? ''));
+                    $templateClassDoc->assign('version',
+                        ($data['version'] ?? ''));
+                    $templateClassDoc->assign('versionRequiredSystem',
+                        ($data['versionRequiredSystem'] ?? ''));
+                    $templateClassDoc->assign('groupAccess',
+                        ($data['groupAccess'] ?? ''));
+
+                    $language = '';
+                    foreach ($data['language'] as $key => $elem) {
+                        $language .= 'language_path_' . $key . ' ' . ($elem['path'] ?? '???') . eol();
+                        $language .= 'language_name_' . $key . ' ' . ($elem['name'] ?? '???') . eol();
+                    }
+
+                    $templateClassDoc->assign('language',
+                        ($data['$language'] ?? ''));
+
+                    if (
+                        $data['generate']['css']
+                    ) {
+                            $templateClassDoc->assign('hasCSS',
+                                                      '* @modul hasCSS ' . ($data['generate']['cssFiles'] ?? ''));
+                    }
+                    else {
+                        $templateClassDoc->assign('hasCSS',
+                                                  '');
+                    }
+
+                    if (
+                        $data['generate']['javascript']
+                    ) {
+                            $templateClassDoc->assign('hasJavascript',
+                                                      '* @modul hasJavascript ' . ($data['generate']['javascriptFiles'] ?? ''));
+                    }
+                    else {
+                        $templateClassDoc->assign('hasJavascript',
+                                                  '');
+                    }
+
+                    $progressData['message'] = 'Create Class ';
+
+                    return $progressData;
+                },
+                    [
+                        '/* $data */' => '$data = ' . var_export($fileContentJson['meta'],
+                                                                 true) . ';'
+                    ]);
+
 
             }
 
--- Skeleton/Skeleton.template.classdoc.tpl
+++ Skeleton/Skeleton.template.classdoc.tpl
@@ -7,10 +7,7 @@
 * @version {$version}
 * @modul versionRequiredSystem {$versionRequiredSystem}
 * @modul groupAccess {$groupAccess}
-* @modul    language_path_de_DE {$language_path_de_DE}
-* @modul    language_name_de_DE {$language_name_de_DE}
-* @modul    language_path_en_US {$language_path_en_US}
-* @modul    language_name_en_US {$language_name_en_US}
+{$language}
 {$hasCSS}
 {$hasJavascript}
 */
